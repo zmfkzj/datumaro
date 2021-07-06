1c4f2ca768372a8ad797597d66ab87daca9eb518
zmfkzj <qlwlal@naver.com>
2021-07-06 15:32:19 +0900

    coco format에서segmentation이 있어도 bbox를읽도록변경

--- datumaro/plugins/coco_format/extractor.py
+++ datumaro/plugins/coco_format/extractor.py
@@ -163,51 +163,51 @@ class _CocoExtractor(SourceExtractor):
                 )
 
             segmentation = ann.get('segmentation')
-            if segmentation and segmentation != [[]]:
-                rle = None
+            # if segmentation and segmentation != [[]]:
+            rle = None
 
-                if isinstance(segmentation, list):
-                    if not self._merge_instance_polygons:
-                        # polygon - a single object can consist of multiple parts
-                        for polygon_points in segmentation:
-                            parsed_annotations.append(Polygon(
-                                points=polygon_points, label=label_id,
-                                id=ann_id, attributes=attributes, group=group
-                            ))
-                    else:
-                        # merge all parts into a single mask RLE
-                        img_h = image_info['height']
-                        img_w = image_info['width']
-                        rles = mask_utils.frPyObjects(segmentation, img_h, img_w)
-                        rle = mask_utils.merge(rles)
-                elif isinstance(segmentation['counts'], list):
-                    # uncompressed RLE
+            if isinstance(segmentation, list):
+                if not self._merge_instance_polygons:
+                    # polygon - a single object can consist of multiple parts
+                    for polygon_points in segmentation:
+                        parsed_annotations.append(Polygon(
+                            points=polygon_points, label=label_id,
+                            id=ann_id, attributes=attributes, group=group
+                        ))
+                else:
+                    # merge all parts into a single mask RLE
                     img_h = image_info['height']
                     img_w = image_info['width']
-                    mask_h, mask_w = segmentation['size']
-                    if img_h == mask_h and img_w == mask_w:
-                        rle = mask_utils.frPyObjects(
-                            [segmentation], mask_h, mask_w)[0]
-                    else:
-                        log.warning("item #%s: mask #%s "
-                            "does not match image size: %s vs. %s. "
-                            "Skipping this annotation.",
-                            image_info['id'], ann_id,
-                            (mask_h, mask_w), (img_h, img_w)
-                        )
+                    rles = mask_utils.frPyObjects(segmentation, img_h, img_w)
+                    rle = mask_utils.merge(rles)
+            elif isinstance(segmentation['counts'], list):
+                # uncompressed RLE
+                img_h = image_info['height']
+                img_w = image_info['width']
+                mask_h, mask_w = segmentation['size']
+                if img_h == mask_h and img_w == mask_w:
+                    rle = mask_utils.frPyObjects(
+                        [segmentation], mask_h, mask_w)[0]
                 else:
-                    # compressed RLE
-                    rle = segmentation
+                    log.warning("item #%s: mask #%s "
+                        "does not match image size: %s vs. %s. "
+                        "Skipping this annotation.",
+                        image_info['id'], ann_id,
+                        (mask_h, mask_w), (img_h, img_w)
+                    )
+            else:
+                # compressed RLE
+                rle = segmentation
 
-                if rle is not None:
-                    parsed_annotations.append(RleMask(rle=rle, label=label_id,
-                        id=ann_id, attributes=attributes, group=group
-                    ))
-            # else:
-                parsed_annotations.append(
-                    Bbox(x, y, w, h, label=label_id,
-                        id=ann_id, attributes=attributes, group=group)
-                )
+            if rle is not None:
+                parsed_annotations.append(RleMask(rle=rle, label=label_id,
+                    id=ann_id, attributes=attributes, group=group
+                ))
+        # else:
+            parsed_annotations.append(
+                Bbox(x, y, w, h, label=label_id,
+                    id=ann_id, attributes=attributes, group=group)
+            )
         elif self._task is CocoTask.labels:
             label_id = self._get_label_id(ann)
             parsed_annotations.append(
